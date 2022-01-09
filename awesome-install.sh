dd6e80a66813da628add9a6b4e1325adbcb45dc1
Johannes Kamprad <archlinux@kamprad.net>
2022-01-09 19:12:40 +0100

    Update awesome-install.sh

--- awesome-install.sh
+++ awesome-install.sh
@@ -4,5 +4,8 @@
 
 mkdir -p ~/.config
 cp -R .config/* ~/.config/
+cp .gtkrc-2.0 ~/.gtkrc-2.0
+cp .nanorc ~/.nanorc
+cp .Xresources ~/.Xresources
 sudo pacman -Syu --needed --noconfirm - < calamares/awesome.txt
 dbus-launch dconf load / < xed.dconf
